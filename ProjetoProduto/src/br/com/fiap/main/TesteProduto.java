ec998f49ebf0e71e2ffccaef3a6c8e99fc3a7939
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-04 11:58:05 -0300

    concluido o getter

--- ProjetoProduto/src/br/com/fiap/main/TesteProduto.java
+++ ProjetoProduto/src/br/com/fiap/main/TesteProduto.java
@@ -1,9 +1,24 @@
 package br.com.fiap.main;
 
+import br.com.fiap.beans.Produto;
+
 public class TesteProduto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// instanciar objeto
+		Produto objetoProduto = new Produto();
+		
+		// setters
+		objetoProduto.setTipo("Celular");
+		objetoProduto.setMarca("Apple");
+		objetoProduto.setQuantidade(1);
+		objetoProduto.setValor(5.000);
+		
+		// getters
+		objetoProduto.getTipo();
+		objetoProduto.getMarca();
+		objetoProduto.getQuantidade();
+		objetoProduto.getValor();
 
 	}
 
