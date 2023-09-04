1656d61760523ad1d9f4b8e0d86a18ff610ea531
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-04 11:25:16 -0300

    visibilidade, tipo de dado e variável

--- ProjetoProduto/src/br/com/fiap/beans/Produto.java
+++ ProjetoProduto/src/br/com/fiap/beans/Produto.java
@@ -1,5 +1,11 @@
 package br.com.fiap.beans;
 
 public class Produto {
+	
+	// visibilidade, tipo de dado e varável
+	private String tipo;
+	private String marca;
+	private int quantidade;
+	private double valor;
 
 }
