7d2a47e453692ff231d404dee07955e1dddc1856
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-04 11:42:32 -0300

    Gerado os setter e getter

--- ProjetoProduto/src/br/com/fiap/beans/Produto.java
+++ ProjetoProduto/src/br/com/fiap/beans/Produto.java
@@ -12,10 +12,34 @@ public class Produto {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
 	// getters
 	public String getTipo() {
 		return tipo;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public double getValor() {
+		return valor;
 	}
 
 }
